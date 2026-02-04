22142ec9de9a072b232a704ea3e873a16974efd2
SLG <slgwfhpersonal@email.com>
2026-02-04 20:16:51 +0530

    changed test file name

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  await page.pause();
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -17,3 +18,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+ 
